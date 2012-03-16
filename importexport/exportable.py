4352f98960fe1aa75416b20de0c19caa763fb7e6
Mark IJbema <markijbema@gmail.com>
2012-03-16 15:13:06 +0100

    split up export_content into before and after

--- importexport/exportable.py
+++ importexport/exportable.py
@@ -1,12 +1,33 @@
 import itertools
+
+def export_all(exports)
+	return list(itertools.chain(*[obj.export() for obj in exports]))
+
 class Exportable():
-    # this function should return a list of subobjects which should be exported if this object is exported
     def export_content(self):
+    	"""
+    	DEPRECATED: use export_after instead
+    	"""
         return []
+
+    def export_before(self):
+    	"""
+    	This function should return a list of all models to export before this model
+    	"""
+        return []
+
+    def export_after(self):
+    	"""
+    	This function should return a list of all models to export after this model
+    	"""
+        return []
+
 
     def export(self):
         return (
+        	export_all(self.export_before())
+            +
             [self]
             +
-            list(itertools.chain(*[obj.export() for obj in self.export_content()]))
+        	export_all(self.export_content() + self.export_after())
         )
